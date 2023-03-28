56298c4f719eedac96d3f427268d01ed3b107169
Harrison Fedor <fedor@temple.edu>
2023-03-28 02:43:16 -0400

    working connection to firebase db
    link application to firebase, set up db, read and write from it with test call

--- app/src/main/java/edu/temple/bistro/MainActivity.kt
+++ app/src/main/java/edu/temple/bistro/MainActivity.kt
@@ -1,6 +1,7 @@
 package edu.temple.bistro
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,11 +11,17 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import edu.temple.bistro.ui.theme.BistroTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        dbTest()
         setContent {
             BistroTheme {
                 // A surface container using the 'background' color from the theme
@@ -26,6 +33,30 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    private fun dbTest() {
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+
+        myRef.addValueEventListener(object: ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = snapshot.value
+                Log.d("READ FROM DB", "Value is: $value")
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+
+        })
+
+        myRef.setValue("BistroDB up and running!")
+        Log.d("STATUS: ", "sent to db")
+
+
+
     }
 }
 
