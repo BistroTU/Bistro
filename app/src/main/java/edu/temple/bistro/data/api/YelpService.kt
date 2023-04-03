b4b5e11acc719be2f9cbba83fe8513353b2e9356
David Fiel <github@dfiel.com>
2023-04-03 00:23:14 -0400

    Add API method to get restaurant details

--- app/src/main/java/edu/temple/bistro/data/api/YelpService.kt
+++ app/src/main/java/edu/temple/bistro/data/api/YelpService.kt
@@ -1,11 +1,19 @@
 package edu.temple.bistro.data.api
 
+import edu.temple.bistro.data.model.Restaurant
 import edu.temple.bistro.data.model.RestaurantSearchResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.QueryMap
 
 interface YelpService {
     @GET("businesses/search")
     fun searchRestaurants(@QueryMap options: Map<String, String>): Call<RestaurantSearchResponse>
+
+    @GET("businesses/{id}")
+    fun getRestaurantDetail(
+        @QueryMap options: Map<String, String>,
+        @Path("id") id: String
+    ): Call<Restaurant>
 }
