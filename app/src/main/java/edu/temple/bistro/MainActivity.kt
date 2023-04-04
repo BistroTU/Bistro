3c5ddad76efe386d03a2ef3e63eae93e510b3376
Harrison Fedor <fedor@temple.edu>
2023-04-03 20:59:34 -0400

    set up location fetching within onCreate
    also includes relevant permission checks

--- app/src/main/java/edu/temple/bistro/MainActivity.kt
+++ app/src/main/java/edu/temple/bistro/MainActivity.kt
@@ -1,5 +1,12 @@
 package edu.temple.bistro
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -16,12 +23,37 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import androidx.core.content.ContextCompat.checkSelfPermission
 import edu.temple.bistro.ui.theme.BistroTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var locationManager: LocationManager
+    private lateinit var locationListener: LocationListener
+    private var currentLocation: Location? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         dbTest()
+
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        locationListener = LocationListener {}
+
+        if (checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+            checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), 123)
+        } else {
+            locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                0L,
+                0f,
+                locationListener
+            )
+            currentLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+        }
+
         setContent {
             BistroTheme {
                 // A surface container using the 'background' color from the theme
@@ -51,14 +83,31 @@ class MainActivity : ComponentActivity() {
             override fun onCancelled(error: DatabaseError) {}
 
         })
-
         myRef.setValue("BistroDB up and running!")
         Log.d("STATUS: ", "sent to db")
+    }
 
-
-
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 123) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    0L,
+                    0f,
+                    locationListener
+                )
+                currentLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            }
+        }
     }
 }
+
 
 @Composable
 fun Greeting(name: String) {
