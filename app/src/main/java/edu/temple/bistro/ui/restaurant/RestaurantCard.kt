bfcbf08928bd048004d45486319a8058aa8869fa
Jacob Snarr <jacobsnarr@gmail.com>
2023-04-26 13:22:53 -0400

    Add sign in screen

--- app/src/main/java/edu/temple/bistro/ui/restaurant/RestaurantCard.kt
+++ app/src/main/java/edu/temple/bistro/ui/restaurant/RestaurantCard.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
---
+++ app/src/main/java/edu/temple/bistro/ui/signin/SignInViewModel.kt
@@ -0,0 +1,38 @@
+package edu.temple.bistro.ui.signin
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import edu.temple.bistro.data.repository.AuthRepository
+import edu.temple.bistro.ui.signin.SignInState
+import edu.temple.bistro.util.Resource
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SignInViewModel @Inject constructor(private val repository: AuthRepository): ViewModel() {
+    val _signInState = Channel<SignInState>()
+    val signInState = _signInState.receiveAsFlow()
+
+    fun signInUser(email: String, password: String) = viewModelScope.launch {
+        repository.loginUser(email, password).collect {result ->
+            when(result) {
+                is Resource.Success -> {
+                    _signInState.send(SignInState(isSuccess = "Sign In Success"))
+                }
+                is Resource.Loading ->{
+                    _signInState.send(SignInState(isLoading = true))
+                }
+                is Resource.Error ->{
+
+                    _signInState.send(SignInState(isError = result.message))
+                }
+            }
+        }
+    }
+
+
+}
