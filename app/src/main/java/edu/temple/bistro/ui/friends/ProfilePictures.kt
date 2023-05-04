846ee97712e53043a2f8b107af72be0667248749
Jacob Snarr <jacobsnarr@gmail.com>
2023-05-04 02:36:10 -0400

    Change PFP to use initials and FriendsScreen

--- app/src/main/java/edu/temple/bistro/ui/friends/ProfilePictures.kt
+++ app/src/main/java/edu/temple/bistro/ui/friends/ProfilePictures.kt
@@ -1,30 +1,34 @@
 package edu.temple.bistro.ui.friends
 
-import android.graphics.drawable.Drawable
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import edu.temple.bistro.R
 
 @Composable
-fun ProfilePicture(imageId: Int) {
-    Image(
-        painter = painterResource(id = imageId),
-        "profile picture",
+fun ProfilePicture(fullName: String) {
+    Box(
         modifier = Modifier
             .clip(RoundedCornerShape(50.dp))
             .border(2.dp, Color.White, CircleShape)
             .size(39.dp)
-        ,
-        contentScale = ContentScale.Crop
-    )
+            .background(Color.White)
+
+
+    ) {
+        Text(getInitials(fullName).toString())
+    }
+}
+
+fun getInitials(fullName: String): List<Char> {
+    return fullName.split(' ').map { it.first() }
 }
